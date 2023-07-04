610b6fb14ae40193acbbee8776ecc6c1f223a78b
Rodrigo Polverari <rodrigo.pp.toledo@gmail.com>
2023-07-04 16:42:00 -0300

    fix landmark swaping windows bug

--- PSI_DRIFT_A.java
+++ PSI_DRIFT_A.java
@@ -5,105 +5,125 @@ import com.github.javacliparser.FloatOption;
 import moa.core.ObjectRepository;
 import moa.tasks.TaskMonitor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-public class PSI_DRIFT extends AbstractChangeDetector {
+public class PSI_DRIFT_A extends AbstractChangeDetector {
 
     private static final long serialVersionUID = -3518369648142099719L;
 
-    public IntOption windowSizeOption = new IntOption(
-            "windowSize",
-            'w',
-            "The size of the window in number of instances.",
+    public IntOption minNumInstancesOption = new IntOption(
+            "minNumInstances",
+            'n',
+            "The number of instances to wait before beginning detection.",
+            100, 0, Integer.MAX_VALUE);
+
+    public IntOption binsOption = new IntOption(
+            "bins",
+            'b',
+            "Number of bins for PSI computation.",
+            10, 1, Integer.MAX_VALUE);
+
+    public IntOption instancesPerDistributionOption = new IntOption(
+            "instancesPerDistribution",
+            'i',
+            "Number of instances per distribution for comparison.",
             100, 1, Integer.MAX_VALUE);
 
     public FloatOption warningLevelOption = new FloatOption(
-            "warningLevel", 'l', "Warning Level.",
+            "warningLevel", 'w', "Warning Level.",
             0.10, 0.0, 1.0);
 
     public FloatOption outcontrolLevelOption = new FloatOption(
             "outcontrolLevel", 'o', "Outcontrol Level.",
             0.20, 0.0, 1.0);
 
-    private int windowSize;
-    private List<Double> windowA;
-    private List<Double> windowB;
-
+    private int m_n;
+    private int minNumInstances;
+    private int numBins;
+    private int instancesPerDistribution;
     private double warningLevel;
     private double outcontrolLevel;
 
-    public PSI_DRIFT() {
+    private LinkedList<Double> firstWindow;
+    private LinkedList<Double> secondWindow;
+
+    public PSI_DRIFT_A() {
         resetLearning();
     }
 
     @Override
     public void resetLearning() {
-        windowSize = this.windowSizeOption.getValue();
+        m_n = 0;
+        minNumInstances = this.minNumInstancesOption.getValue();
+        numBins = this.binsOption.getValue();
+        instancesPerDistribution = this.instancesPerDistributionOption.getValue();
         warningLevel = this.warningLevelOption.getValue();
         outcontrolLevel = this.outcontrolLevelOption.getValue();
-        windowA = new ArrayList<>();
-        windowB = new ArrayList<>();
+        firstWindow = new LinkedList<>();
+        secondWindow = new LinkedList<>();
     }
 
     @Override
     public void input(double prediction) {
-        if (windowA.size() < windowSize) {
-            windowA.add(prediction);
-        } else if (windowB.size() < windowSize) {
-            windowB.add(prediction);
-        } else {
-            double psi = computePSI(windowA, windowB);
+        m_n++;
 
-            this.estimation = psi;
-            this.isChangeDetected = false;
-            this.isWarningZone = false;
-            this.delay = 0;
+        if (m_n <= minNumInstances) {
+            return;
+        }
 
-            if (psi > outcontrolLevel) {
-                this.isChangeDetected = true;
-            } else if (psi > warningLevel) {
-                this.isWarningZone = true;
-            }
+        if (m_n <= minNumInstances + instancesPerDistribution) {
+            firstWindow.add(prediction);
+            return;
+        }
 
-            // RESET e SWAP
-            windowA = windowB;
-            windowB = new ArrayList<>();
-            windowB.add(prediction);
+        secondWindow.add(prediction);
+
+        if (secondWindow.size() < instancesPerDistribution) {
+            return;
+        }
+
+        double psi = computePSI(firstWindow, secondWindow, numBins);
+
+        updateDetectionStatus(psi);
+
+        firstWindow = secondWindow;
+        secondWindow = new LinkedList<>();
+    }
+
+    private void updateDetectionStatus(double psi) {
+        this.estimation = psi;
+        this.isChangeDetected = false;
+        this.isWarningZone = false;
+        this.delay = 0;
+        if (psi > outcontrolLevel) {
+            this.isChangeDetected = true;
+        } else if (psi > warningLevel) {
+            this.isWarningZone = true;
         }
     }
 
-    private double computePSI(List<Double> expected, List<Double> actual) {
-        int numBins = 10;
+    private double computePSI(List<Double> expected, List<Double> actual, int numBins) {
         double minValue = Collections.min(expected);
         double maxValue = Collections.max(expected);
         double binWidth = (maxValue - minValue) / numBins;
 
-        
         double[] expectedDist = new double[numBins];
         double[] actualDist = new double[numBins];
-        for (double val : expected) {
-            int binIndex = Math.min((int) ((val - minValue) / binWidth), numBins - 1);
-            expectedDist[binIndex]++;
+        for (Double value : expected) {
+            int binIndex = Math.min((int) ((value - minValue) / binWidth), numBins - 1);
+            expectedDist[binIndex] += 1;
         }
-        for (double val : actual) {
-            int binIndex = Math.min((int) ((val - minValue) / binWidth), numBins - 1);
-            actualDist[binIndex]++;
+        for (Double value : actual) {
+            int binIndex = Math.min((int) ((value - minValue) / binWidth), numBins - 1);
+            actualDist[binIndex] += 1;
         }
 
-        // Normalização
+        double psi = 0.0;
         for (int i = 0; i < numBins; i++) {
             expectedDist[i] /= expected.size();
             actualDist[i] /= actual.size();
-        }
-
-        // Cálculo PSI
-        double psi = 0;
-        for (int i = 0; i < numBins; i++) {
-            if (expectedDist[i] != 0) {
-                double actualRate = actualDist[i] == 0 ? 0.001 : actualDist[i]; // Avoid division by zero
-                psi += (expectedDist[i] - actualRate) * Math.log(expectedDist[i] / actualRate);
+            if (expectedDist[i] > 0.0 && actualDist[i] > 0.0) {
+                psi += (expectedDist[i] - actualDist[i]) * Math.log(expectedDist[i] / actualDist[i]);
             }
         }
 
@@ -112,12 +132,24 @@ public class PSI_DRIFT extends AbstractChangeDetector {
 
     @Override
     public void getDescription(StringBuilder sb, int indent) {
-        // TODO Auto-generated method stub
+        // Append the description of the drift detection algorithm to the StringBuilder
+       
+        StringBuilder newLineIndent = new StringBuilder();
+        for (int i = 0; i < indent; i++) {
+            newLineIndent.append("\n");
+        }
+        sb.append(newLineIndent).append("PSI_DRIFT_B: Detects concept drift based on the PSI measure.");
+        sb.append(newLineIndent).append("Parameters:");
+        sb.append(newLineIndent).append("- minNumInstances: The number of instances to wait before beginning detection.");
+        sb.append(newLineIndent).append("- bins: Number of bins for PSI computation.");
+        sb.append(newLineIndent).append("- instancesPerDistribution: Number of instances per distribution for comparison.");
+        sb.append(newLineIndent).append("- warningLevel: Warning level for drift detection.");
+        sb.append(newLineIndent).append("- outcontrolLevel: Outcontrol level for drift detection.");
     }
 
     @Override
-    protected void prepareForUseImpl(TaskMonitor monitor, ObjectRepository repository) {
-        // TODO Auto-generated method stub
+    protected void prepareForUseImpl(TaskMonitor monitor,
+                                      ObjectRepository repository) {
     }
 }
 
